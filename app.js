e00bfbf2390505d933f1b5e454e37620c0d5d2a2
dearests <soguazu@gmail.com>
2019-03-06 23:11:26 +0100

    project requirement completed,but remaining minor modification

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const utility = require('./utility/utility');
 const StateHadnler = require('./stateHandler/stateHandler');
 
 function createDataObj(arrData, id) {
+    if ((utility.getLastProductId + 1) === (id + 1)) process.kill(process.pid);
     let data = {};
     let temp = arrData.split(' ');
     data.id = id + 1;
     data.product = temp[0] + ' ' + temp[1];
     data.state = temp[2];
+    
     return data;
 }
 
@@ -14,12 +16,14 @@ async function app() {
     let rawProductData = await utility.read('./database/Product-Input');
     let cleanProductData = await utility.clean(rawProductData);
 
-    utility.setLastProductId(cleanProductData.length - 1)
+    utility.setLastProductId(cleanProductData.length)
+    // console.log(utility.islastProduct)
+    console.log(cleanProductData.length)
+    console.log(utility.getLastProductId)
 
     try {
         for (let i = 0; i <= cleanProductData.length; i++) {
             let data = createDataObj(cleanProductData[i], i);
-            
             const stateHadnler = new StateHadnler(data);
             stateHadnler.start();
         }
--- stages/producer/producer.js
+++ stages/producer/producer.js
@@ -8,11 +8,12 @@ class Producer {
     go(product, id) {
         let logData = `Product: ${product} | from: ${this.currentState} | to: ${this.nextState}\n`;
         console.log(id);
-        if (id === allExports.Utility.getLastProductId) {
-           allExports.Utility.islastProduct = true;
-        }
+        
         allExports.Utility.write('./database/Product-output', logData).then(() => {
             console.log(logData);
+            if (allExports.Utility.getLastProductId === id) {
+                allExports.Utility.islastProduct = true;
+            }
 
             this.state.nextStage(new allExports[this.nextState](this.state));
             
